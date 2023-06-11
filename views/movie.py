da865f7008703869d6b4ff0f4ccda8a793a29984
Ananya-Bhushan <85629090+Ananya-Bhushan@users.noreply.github.com>
2023-06-11 21:42:43 +0530

    Add files via upload

---
+++ views/movie.py
@@ -0,0 +1,20 @@
+from flask import jsonify
+#from models.movie import Movie
+from flask import Blueprint
+
+movie_api = Blueprint('movie_api', __name__)
+
+@movie_api.route('/v1/movie/<int:movie_id>', methods=['GET'])
+def get_movie(movie_id):
+    # movie = Movie.query.get(movie_id)
+    # if movie:
+    #     return jsonify({
+    #         'id': movie.id,
+    #         'title': movie.title,
+    #         'poster_path': movie.poster_path,
+    #         'language': movie.language,
+    #         'overview': movie.overview,
+    #         'release_date': movie.release_date.isoformat()
+    #     })
+    # else:
+    return jsonify({'error': 'Movie not found'}), 404
