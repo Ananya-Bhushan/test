f6317f1de5699daf10d9abd561c6c4bfdc84e349
Ananya-Bhushan <85629090+Ananya-Bhushan@users.noreply.github.com>
2023-06-13 01:13:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,20 +16,5 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-# @app.route('/v1/movie/<int:movie_id>', methods=['GET'])
-# def get_movie(movie_id):
-#     # movie = Movie.query.get(movie_id)
-#     # if movie:
-#     #     return jsonify({
-#     #         'id': movie.id,
-#     #         'title': movie.title,
-#     #         'poster_path': movie.poster_path,
-#     #         'language': movie.language,
-#     #         'overview': movie.overview,
-#     #         'release_date': movie.release_date.isoformat()
-#     #     })
-#     # else:
-#     return jsonify({'error': 'Movie not found'}), 404
-
 if __name__ == '__main__':
     app.run()
