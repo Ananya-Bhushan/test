2f5a4190cd377adabcf20fc1df83871c3a848a26
Ananya-Bhushan <85629090+Ananya-Bhushan@users.noreply.github.com>
2023-06-13 01:32:37 +0530

    Update movie.py

--- controllers/movie.py
+++ controllers/movie.py
@@ -3,34 +3,20 @@ from app import app
 from models.movie import Movie
 from flask import jsonify, request
 
-app = Flask(__name__)
 
-class controller:
- def __init__(self):
-   pass
-
- def get_movie(movie_id):
-     movie = Movie.query.get(movie_id)
-     if movie:
-         return jsonify({
-             'id': movie.id,
-             'title': movie.title,
-             'poster_path': movie.poster_path,
-             'language': movie.language,
-             'overview': movie.overview,
-             'release_date': movie.release_date.isoformat()
-         })
-     else:
-         return jsonify({'error': 'Movie not found'}), 404
-      
-   @app.route('/v1/movie/<int:movie_id>', methods=['GET'])
-   def get_movie(movie_id):
-    Controller=controller(movie_id)
-    return Controller.get_movie(movie_id)
-   
-   
-   if __name__ == '__main__':
-    app.run()
-   
+@app.route('/v1/movie/<int:movie_id>', methods=['GET'])
+def get_movie(movie_id):
+    movie = Movie.query.get(movie_id)
+    if movie:
+        return jsonify({
+            'id': movie.id,
+            'title': movie.title,
+            'poster_path': movie.poster_path,
+            'language': movie.language,
+            'overview': movie.overview,
+            'release_date': movie.release_date.isoformat()
+        })
+    else:
+        return jsonify({'error': 'Movie not found'}), 404
 
 
